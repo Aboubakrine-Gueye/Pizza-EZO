9058050a40cfaa78ade3f93a08417581b2aaefaf
Aboubakrine-Gueye <aboubag@hotmail.com>
2022-09-07 13:18:42 -0400

    Aboubakrine Gueye - Done

--- client/src/components/UserOrder.js
+++ client/src/components/UserOrder.js
@@ -1,22 +1,9 @@
 import React from 'react';
-Cards from 'react-credit-cards';
-import "react-credit-cards/lib/styles.scss";
-import "react-credit-card/es/styles-compiled.css";
-import styled from "styled-components";
-
-
+import styled from 'styled-components';
 
 const UserOrder = () => {
-
-  const [number, setNumber] = useState('');
-  const [name, setName] = useState('');
-  const [expiry, setExpiry] = useState('');
-  const [cvv, setCvv] = useState('');
-  const [focus, setFocus] = useState('');
-  return
-  (
-    
-  ) <div>UserOrder</div>;
+  return;
+  <div>UserOrder</div>;
 };
 
 export default UserOrder;
